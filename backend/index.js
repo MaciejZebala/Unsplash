279465df74fd1bb27bc476a83f3661e560b4b6e5
MaciejZebala <kropka551@op.pl>
2021-02-28 19:08:23 +0100

    Changes for heroku v4

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// app.use(cors());
+app.use(cors());
 const buildPath = path.join(__dirname, '..', 'build');
 // const sos = path.join(__dirname, '..', 'build/index.html');
 // console.log(sos);
